52752f977028c71e5a0fcd290749ae8bef9861a0
MonsieurDuck <dunstanwang@gmail.com>
2023-07-07 22:01:32 -0700

    adding extremely basic ui and timer bar for buttonmashing
    made a win and loss condition in script
    added timer bar

--- Assets/Scripts/Util/ButtonMasher.cs
+++ Assets/Scripts/Util/ButtonMasher.cs
@@ -1,37 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonMasher : MonoBehaviour
 {
     public Transform container;
     public Transform tracker;
+    public Image fillBar;
     public float containerHeight;
     public float rateOfDescent;
     public float rateOfAscent;
+    public float time;
+
+    private float timeRemaining;
+    private float fillBarHeight;
+    private bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         containerHeight = container.localScale.y;
         tracker = gameObject.GetComponent<Transform>();
+        timeRemaining = time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(tracker.position.y > -1.0 * containerHeight/2.0f) {
-            tracker.position -= new Vector3(0, containerHeight * rateOfDescent, 0);
-        }
         
-        if (Input.GetButtonDown("Fire1")){
-            if (tracker.position.y + containerHeight * rateOfAscent > containerHeight/2.0f) {
-                tracker.position = new Vector3(tracker.position.x, containerHeight/2.0f, tracker.position.z);
+        if (timeRemaining > 0 && !gameOver) {
+
+            timeRemaining -= Time.deltaTime;
+            fillBar.fillAmount -= 1.0f / time * Time.deltaTime;
+
+            if(tracker.position.y > -1.0 * containerHeight/2.0f) {
+                tracker.position -= new Vector3(0, containerHeight * rateOfDescent * Time.deltaTime, 0);
             } else {
-                tracker.position += new Vector3(0, containerHeight * rateOfAscent, 0);
+                gameOver = true;
             }
-            
-            Debug.Log(containerHeight * rateOfAscent);
+        
+            if (Input.GetButtonDown("Fire1")){
+                if (tracker.position.y + containerHeight * rateOfAscent > containerHeight/2.0f) {
+                    tracker.position = new Vector3(tracker.position.x, containerHeight/2.0f, tracker.position.z);
+                    gameOver = true;
+                } else {
+                    tracker.position += new Vector3(0, containerHeight * rateOfAscent, 0);
+                }
+            }
+
+            //very temp testing win condition
+            if(Input.GetKeyDown("space")) {
+                timeRemaining = 0;
+            }
+
+        } else if (timeRemaining <= 0 && !gameOver){
+            Debug.Log("Survived!");
+            gameObject.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        } else {
+            Debug.Log("You got caught!");
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 }
