7b3285d5eb3c17f11b40bd40433c1a6cb9620554
Collin Eng <eng.engineereng@gmail.com>
2023-07-08 21:41:22 -0700

    lettuce face added

--- Assets/LettuceAnimatorController.cs
+++ Assets/LettuceAnimatorController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class LettuceAnimatorController : MonoBehaviour
 {
-    private Animation animator;
+    private Rigidbody2D rigidbody2D;
+    public Animator AnimationController;
+    [SerializeField] float animationSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        // animator
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float newSpeed = Mathf.Clamp(Mathf.Abs(rigidbody2D.velocity.x), 0, 1.5f);
+        // Debug.Log("new speed:" + newSpeed);
+        AnimationController.SetFloat("Speed", newSpeed);
+        AnimationController.SetBool("FacingRight", rigidbody2D.velocity.x >= 0);
     }
 }
