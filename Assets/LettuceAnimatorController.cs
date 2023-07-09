1c116274076fc0d2e1c6b826f951870b71f286e6
Collin Eng <eng.engineereng@gmail.com>
2023-07-08 20:17:40 -0700

    lettuce sprites and heart added

---
+++ Assets/LettuceAnimatorController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LettuceAnimatorController : MonoBehaviour
+{
+    private Animation animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // animator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
