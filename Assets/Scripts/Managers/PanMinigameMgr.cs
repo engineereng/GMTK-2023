bb923c7654585b89276e6f8f4e37729f4a4191cd
Collin Eng <eng.engineereng@gmail.com>
2023-07-09 03:47:14 -0700

    fix: key label, feat: tutorial timing

--- Assets/Scripts/Managers/PanMinigameMgr.cs
+++ Assets/Scripts/Managers/PanMinigameMgr.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class PanMinigameMgr : MonoBehaviour
 {
+    public GameObject TutorialOverlay;
+    public GameObject WinOverlay;
+    [SerializeField] private float tutorialDuration;
+
+    public Timer timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer.enabled = false;
+        ShowTutorial();
+        Invoke(nameof(HideTutorial), tutorialDuration);
+    }
+
+    void HideTutorial()
+    {
+        TutorialOverlay.SetActive(false);
+        Invoke(nameof(StartTimer), 0.8f);
+    }
+
+    void StartTimer()
+    {
+        timer.enabled = true;
+    }
+
+    void ShowTutorial()
+    {
+        WinOverlay.SetActive(false);
+        TutorialOverlay.SetActive(true);
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    public void Win()
+    {
+        WinOverlay.SetActive(true);
+        GameMgr.Instance.waitAndLoadNextScene(5.0f);
     }
 
     // Update is called once per frame
