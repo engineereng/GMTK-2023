a2d2fcc2be6a1f91395958ab39f6d1da58f419c9
Brayden Shinkawa <braydenshinkawa@gmail.com>
2023-07-09 02:16:11 -0700

    even more necessary fmod plugins

---
+++ Assets/Scripts/Managers/FMODEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Sizzling")]
+    [field: SerializeField] public EventReference sizzling { get; private set; }
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+    [field: Header("Knife SFX")]
+    [field: SerializeField] public EventReference knifeSlice { get; private set; }
+    [field: Header("Chips")]
+    [field: SerializeField] public EventReference chips { get; private set; }
+    [field: Header("Success")]
+    [field: SerializeField] public EventReference success { get; private set; }
+    [field: Header("Failure")]
+    [field: SerializeField] public EventReference failure { get; private set; }
+
+    public static FMODEvents instance { get; private set; }
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events instance in the scene");
+        }
+        instance = this;
+    }
+}
